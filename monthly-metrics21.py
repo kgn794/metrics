2217980cbeb3f00f55a4db81857bd9ab8998e92f
kgn794 <170333726+kgn794@users.noreply.github.com>
2026-07-05 18:04:24 +0530

    Update and rename monthly-metrics20.py to monthly-metrics21.py

--- monthly-metrics21.py
+++ monthly-metrics21.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 import streamlit as st
 import plotly.graph_objects as go
-import release_count
+from release_count import get_release_count
 
 
-release_count = get_release_count()
+rel_cnt = get_release_count()
 # Layout
 row1 = st.container()
 with row1:
-    st.plotly_chart(release_count, use_container_width=True)
+    st.plotly_chart(rel_cnt, use_container_width=True)
 
     
